a614508f173d40e9cc30aa5457d0a89f34fdf88b
cristhian <cristhian.cabezas04@gmail.com>
2024-01-16 13:29:21 -0500

    modificar view

--- papeleria/views/__init__.py
+++ papeleria/views/__init__.py
@@ -2,4 +2,4 @@ from .viewRegistro import ViewRegistro
 from .viewUserDetail import UserDetailView
 from .viewUserCreate import UserCreateView
 from .viewVentas import ViewVentas
-from .viewRegistrosUsuarioNoAut import ViewRegistroNoAut
+from .viewRegistroNoAut import ViewRegistroNoAut
